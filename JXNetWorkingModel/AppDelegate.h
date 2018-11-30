0f924ad93370589cb831f36ec05d9a8559271e2e
Jiles <632036801@qq.com>
2018-11-30 16:18:46 +0800

    Initial Commit

---
+++ JXNetWorkingModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JXNetWorkingModel
+//
+//  Created by 徐沈俊杰 on 2018/11/30.
+//  Copyright © 2018年 JX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
